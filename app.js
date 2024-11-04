abf77b813b393f5e31aa440f2c8732340cbe058e
Hamzhgg <gharibehhamzh@gmail.com>
2024-11-04 16:03:26 +0300

    all

---
+++ app.js
@@ -0,0 +1,11 @@
+const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+
+const petsArray = ['Rover', 'Snuffles', 'Toto', 'Spot'];
+
+const [firstPet, secondPet] = petsArray;
+
+console.log(firstPet);
+console.log(secondPet);
+
+console.log(petsArray[0]);
+console.log(petsArray[1]);
